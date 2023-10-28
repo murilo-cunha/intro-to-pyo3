5e02e2600776ac6622dea4100a43b9ab796f60bb
Murilo Cunha <murilo@dataroots.io>
2023-10-28 17:53:32 +0200

    add padding helper func

--- sobel_rs/src/lib.rs
+++ sobel_rs/src/lib.rs
@@ -1,3 +1,4 @@
+use ndarray::s;
 use pyo3::{exceptions::PyValueError, prelude::*};
 
 #[pyclass]
@@ -22,6 +23,15 @@ impl Matrix {
     }
 }
 
+fn pad_with_zeros(image: ndarray::Array2<u8>, padding: usize) -> ndarray::Array2<u8> {
+    let (y, x) = image.dim();
+    let mut padded_image = ndarray::Array2::zeros((y + 2 * padding, x + 2 * padding));
+    padded_image
+        .slice_mut(s![padding..y + padding, padding..x + padding])
+        .assign(&image);
+    padded_image
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn sobel_rs(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -43,4 +53,23 @@ fn sobel_rs(_py: Python, m: &PyModule) -> PyResult<()> {
 
     m.add_class::<Matrix>()?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pad_with_zeros() {
+        let input_img = ndarray::arr2(&[[1, 2], [3, 4]]);
+        let expected_img = ndarray::arr2(&[
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 2, 0, 0],
+            [0, 0, 3, 4, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ]);
+        assert_eq!(pad_with_zeros(input_img, 2), expected_img);
+    }
 }
